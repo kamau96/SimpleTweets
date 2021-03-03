11a15b6fbbe70dc4065628a358350677c3412b4b
kamau96 <bkw5279@psu.edu>
2021-03-02 23:08:33 -0500

    Twitter part 2

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,18 +1,36 @@
 package com.codepath.apps.restclienttemplate;
 
+<<<<<<< HEAD
+=======
+import androidx.annotation.Nullable;
+>>>>>>> ce63a78... Twitter part 2
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+<<<<<<< HEAD
 import android.os.Bundle;
 import android.util.Log;
+=======
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+>>>>>>> ce63a78... Twitter part 2
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+<<<<<<< HEAD
+=======
+import org.parceler.Parcels;
+>>>>>>> ce63a78... Twitter part 2
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +39,11 @@ import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
     public static final String TAG = "TimelineActivity";
+<<<<<<< HEAD
+=======
+    private final int REQUEST_CODE = 20;
+
+>>>>>>> ce63a78... Twitter part 2
     TwitterClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
@@ -85,6 +108,37 @@ public class TimelineActivity extends AppCompatActivity {
         }, tweets.get(tweets.size() - 1).id);
     }
 
+<<<<<<< HEAD
+=======
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.compose){
+            Toast.makeText(this,"Compose!",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            tweets.add(0,tweet);
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+>>>>>>> ce63a78... Twitter part 2
     private void populateHomeTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
